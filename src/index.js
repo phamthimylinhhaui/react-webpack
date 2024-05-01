8502d804f5079db2ef8aed2e1440c1619668e7dc
hapo-linhptm <linh.ptm@haposoft.com>
2024-05-02 00:03:16 +0700

    base config react and install some library

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return (
+    <div>
+      <h1>xin chao</h1>
+      <h1>xin chao</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
